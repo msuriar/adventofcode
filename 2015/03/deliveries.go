232d0a727faf0dee55394c90388e2d453f639093
Murali Suriar <code@suriar.com>
2023-07-05 23:39:21 +0100

    Solve part 1.

--- 2015/03/deliveries.go
+++ 2015/03/deliveries.go
@@ -1,18 +1,23 @@
 package main
 
-type Point struct{
+import (
+	"fmt"
+	"os"
+)
+
+type Point struct {
 	x, y int
 }
 
-type Course struct{
+type Course struct {
 	position Point
-	counts map[Point]int
+	counts   map[Point]int
 }
 
 func NewCourse() *Course {
 	c := &Course{
 		position: Point{x: 0, y: 0},
-		counts: map[Point]int{},
+		counts:   map[Point]int{},
 	}
 	c.counts[c.position] += 1
 
@@ -23,20 +28,33 @@ func (c *Course) Navigate(tc string) {
 	for _, r := range tc {
 		switch r {
 		case '>':
-		c.position.x += 1
+			c.position.x += 1
 		case 'v':
-		c.position.y -= 1
+			c.position.y -= 1
 		case '<':
-		c.position.x -= 1
+			c.position.x -= 1
 		case '^':
-		c.position.y += 1
+			c.position.y += 1
 		}
 
 		c.counts[c.position] += 1
 	}
-	
+
 }
 
 func (c *Course) UniqueHouses() int {
 	return len(c.counts)
+}
+
+func main() {
+	c := NewCourse()
+
+	b, err := os.ReadFile("input.txt")
+	if err != nil {
+		panic(err)
+	}
+
+	c.Navigate(string(b))
+
+	fmt.Printf("Single worker, unique houses: %d\n", c.UniqueHouses())
 }
